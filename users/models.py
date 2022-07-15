30d258c2f7dd8d3c78cdd83d5c0ea36eb191b692
ST_DOMAIN\Kirpichenka_AP <Kirpichenka_AP@outlook.com>
2022-07-15 14:00:57 +0300

    fixed create user func

--- users/models.py
+++ users/models.py
@@ -18,7 +18,19 @@ class CustomAccountManager(BaseUserManager):
             raise ValueError(
                 'Superuser must be assigned to is_superuser=True.')
 
-        return self.create_user(self, email, user_name, first_name, password, **other_fields)
+        return self.create_user(email, user_name, first_name, password, **other_fields)
+
+    def create_user(self, email, user_name, first_name, password, **other_fields):
+
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name,
+                          first_name=first_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
 
     def create_user(self, email, user_name, first_name, password, **other_fields):
 
