f56848cc75e8e8f9d231c20eb6ad85632062c017
ST_DOMAIN\Kirpichenka_AP <Kirpichenka_AP@outlook.com>
2022-07-15 12:51:25 +0300

    create user & superuser

--- users/models.py
+++ users/models.py
@@ -4,7 +4,34 @@ from django.utils import timezone
 
 
 class CustomAccountManager(BaseUserManager):
-    ...
+
+    def create_user(self, email, user_name, first_name, password, **other_fields):
+
+        if not email:
+            raise ValueError('You must provide an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name,
+                          first_name=first_name, **other_fields)
+
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, user_name, first_name, password, **other_fields):
+
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is+superuser=True.')
+
+        return self.create_user(self, email, user_name, first_name, password, **other_fields)
 
 
 class NewUser(AbstractBaseUser, PermissionsMixin):
@@ -17,11 +44,11 @@ class NewUser(AbstractBaseUser, PermissionsMixin):
     about = models.TextField(verbose_name='about', max_length=500, blank=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
-    
+
     objects = CustomAccountManager()
-    
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name']
-    
+
     def __str__(self):
         return self.user_name
